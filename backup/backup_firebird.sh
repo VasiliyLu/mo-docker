82fbac5f6a5229165b68ff38bffe9c7eed6e39ea
Vasiliy Lutsenko <vasiliy.lu@gmail.com>
2024-07-09 11:41:53 +0200

    fixed backup logging

--- backup/backup_firebird.sh
+++ backup/backup_firebird.sh
@@ -5,6 +5,7 @@ BACKUP_DIR=/backup
 
 echo "Backup DB $DB_NAME to $BACKUP_DIR/$BACKUP_FN"
 
-LOG_FN=$BACKUP_DIR/backup_`date +'%Y%m%d_%H%M%S'`.log
+LOG_FN=$BACKUP_DIR/backup.log
+rm -f $LOG_FN
 /usr/local/firebird/bin/gbak -b -g -y $LOG_FN -v -user $DB_USER -pass $DB_PASS $DB_NAME /tmp/$BACKUP_FN
 tar -I pbzip2 -c --remove-files --directory=/tmp $BACKUP_FN | openssl enc -e -aes256 -out $BACKUP_DIR/$BACKUP_FN.tar.bz2.enc -k $BACKUP_PASS
